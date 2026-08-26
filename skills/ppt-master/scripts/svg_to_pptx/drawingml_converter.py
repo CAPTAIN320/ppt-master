7e58432c65bbca11cac9b9552bcbefb4d8d6e9c1
Yaqub Mahmoud <yaqub320@gmail.com>
2026-08-27 01:59:29 +0900

    Update drawingml_converter.py

--- skills/ppt-master/scripts/svg_to_pptx/drawingml_converter.py
+++ skills/ppt-master/scripts/svg_to_pptx/drawingml_converter.py
@@ -286,7 +286,7 @@ def convert_g(elem: ET.Element, ctx: ConvertContext) -> ShapeResult | None:
 # Defs collection & element dispatch
 # ---------------------------------------------------------------------------
 
-_NON_VISUAL_TAGS = frozenset(('defs', 'title', 'desc', 'metadata', 'style', 'use'))
+_NON_VISUAL_TAGS = frozenset(('defs', 'title', 'desc', 'metadata', 'style', 'use', 'clipPath'))
 
 
 def _supports_matrix_transform(elem: ET.Element) -> bool:
